c30ad10d6e8743fa0bdc6bed2a80cfdcd62a6543
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-08-01 11:26:59 +0100

    chore: update code to log response and requests

--- kong/plugins/treblle/handler.lua
+++ kong/plugins/treblle/handler.lua
@@ -25,79 +25,22 @@ local plugin = {
 
 
 
--- handles more initialization, but AFTER the worker process has been forked/created.
--- It runs in the 'init_worker_by_lua_block'
-function plugin:init_worker()
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'init_worker' handler")
-
-end --]]
-
-
-
---[[ runs in the 'ssl_certificate_by_lua_block'
--- IMPORTANT: during the `certificate` phase neither `route`, `service`, nor `consumer`
--- will have been identified, hence this handler will only be executed if the plugin is
--- configured as a global plugin!
-function plugin:certificate(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'certificate' handler")
-
-end --]]
-
-
-
---[[ runs in the 'rewrite_by_lua_block'
--- IMPORTANT: during the `rewrite` phase neither `route`, `service`, nor `consumer`
--- will have been identified, hence this handler will only be executed if the plugin is
--- configured as a global plugin!
-function plugin:rewrite(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'rewrite' handler")
-
-end --]]
-
-
-
--- runs in the 'access_by_lua_block'
-function plugin:access(plugin_conf)
-
-  -- your custom code here
-  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
-
-end --]]
-
-
--- runs in the 'header_filter_by_lua_block'
-function plugin:header_filter(plugin_conf)
-
-  -- your custom code here, for example;
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
-
-end --]]
-
-
---[[ runs in the 'body_filter_by_lua_block'
-function plugin:body_filter(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'body_filter' handler")
-
-end --]]
-
-
---[[ runs in the 'log_by_lua_block'
 function plugin:log(plugin_conf)
+  -- Log the request details
+  kong.log.debug("Request: ", {
+    method = kong.request.get_method(),
+    uri = kong.request.get_path(),
+    headers = kong.request.get_headers(),
+    body = kong.request.get_body(),
+  })
 
-  -- your custom code here
-  kong.log.debug("saying hi from the 'log' handler")
-
-end --]]
-
+  -- Log the response details
+  kong.log.debug("Response: ", {
+    status = kong.response.get_status(),
+    headers = kong.response.get_headers(),
+    body = kong.response.get_body(),
+  })
+end
 
 -- return our plugin object
 return plugin
--- spec/treblle/01-unit_spec.lua
+++ spec/treblle/01-unit_spec.lua
@@ -1,10 +1,11 @@
-local PLUGIN_NAME = "myplugin"
+local PLUGIN_NAME = "treblle"
 
 
 -- helper function to validate data against a schema
-local validate do
+local validate
+do
   local validate_entity = require("spec.helpers").validate_plugin_config_schema
-  local plugin_schema = require("kong.plugins."..PLUGIN_NAME..".schema")
+  local plugin_schema = require("kong.plugins." .. PLUGIN_NAME .. ".schema")
 
   function validate(data)
     return validate_entity(data, plugin_schema)
@@ -13,33 +14,12 @@ end
 
 
 describe(PLUGIN_NAME .. ": (schema)", function()
-
-
-  it("accepts distinct request_header and response_header", function()
+  it("accepts distinct api_key and project_id", function()
     local ok, err = validate({
-        request_header = "My-Request-Header",
-        response_header = "Your-Response",
-      })
+      api_key = "Treblle-API-Key",
+      project_id = "Treblle-Project-ID",
+    })
     assert.is_nil(err)
     assert.is_truthy(ok)
   end)
-
-
-  it("does not accept identical request_header and response_header", function()
-    local ok, err = validate({
-        request_header = "they-are-the-same",
-        response_header = "they-are-the-same",
-      })
-
-    assert.is_same({
-      ["config"] = {
-        ["@entity"] = {
-          [1] = "values of these fields must be distinct: 'request_header', 'response_header'"
-        }
-      }
-    }, err)
-    assert.is_falsy(ok)
-  end)
-
-
 end)
